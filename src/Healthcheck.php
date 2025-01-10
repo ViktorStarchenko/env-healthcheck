4ffd63c2d0feba00149d0e88586493180e5f0b44
Viktor Starchenko <viktor.s@overdose.digital>
2025-01-10 17:03:35 +0200

    updated Healthcheck class

--- src/Healthcheck.php
+++ src/Healthcheck.php
@@ -11,21 +11,11 @@ class Healthcheck {
     }
 
     public function handle_healthcheck() {
-        // Checking the environment
-        $is_production = getenv('WP_ENV') === 'production';
 
-        if (!$is_production) {
-            return new \WP_REST_Response([
-                'status'  => 'fail',
-                'message' => 'Site is NOT in production mode',
-                "mode" => "development"
-            ], 200);
-        }
+        $mode = getenv('WP_ENV') === 'production' ? 'production' : 'development';
 
-        return new \WP_REST_Response([
-            'status'  => 'success',
-            'message' => 'Site is up and running in PRODUCTION mode',
-            "mode" => "production"
-        ], 200);
+        $response = ['mode' => $mode];
+
+        return new \WP_REST_Response($response, 200, ['Content-Type' => 'application/json']);
     }
 }
